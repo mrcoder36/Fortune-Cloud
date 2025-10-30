d2ffec13d66df57adefc1fc484d040b1244000ce
mrcoder36 <mr.coder36@gmail.com>
2025-10-30 17:06:27 +0530

    space

--- logic/pattern_1.c
+++ logic/pattern_1.c
@@ -4,7 +4,7 @@ int main() {
     for (int i = 1 ; i < 8; i++) {
         
         for (int j = 0; j < 5; j++) {
-            if (i %2 == 0){
+            if (i %2 == 0) {
                 printf("B ");
             } else {
                 printf("A ");

037a031aeecfd5b59cd71cbb0c850918be60d7f8
mrcoder36 <mr.coder36@gmail.com>
2025-11-02 19:45:45 +0530

    Refactor pattern printing code and add new file for testing

--- logic/numPattern.c
+++ logic/numPattern.c
@@ -8,8 +8,8 @@ void main()
     int count = 0;
     for(int i = 0; i < 5; i++) {
         for(int j = 0; j < 5; j++) {
-            count++;
-            printf("%d  ",count);
+            printf("* ");
+            
         }
         printf("\n");
     }
---
+++ logic/tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+for (int i = 1 ; i < 4; i++) {
+        
+        for (int j = 0; j < 5; j++) {
+            printf("A ");
+        }
+        printf("\n");

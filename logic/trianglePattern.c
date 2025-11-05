c640569faab6cfbda622efadecc291450400bfdf
mrcoder36 <mr.coder36@gmail.com>
2025-11-05 15:42:00 +0530

    Add triangle pattern generation programs in C

---
+++ logic/trianglePattern.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main() {
+    int r, c;
+    for(r=1; r<=4; r++) {
+        for(c=1; c<=r; c++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
---
+++ logic/trianglePattern_2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main() {
+    int r, c;
+    for(r=1; r<=4; r++) {
+        for(c=1; c<=r; c++) {
+            if(r%2==0){
+                printf("0 ");
+            } else {
+                printf("1 ");
+            }
+        }
+        printf("\n");
+    }
+}

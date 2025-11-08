c071daf798cc90680405ade6760a9ae0e9f5c524
mrcoder36 <mr.coder36@gmail.com>
2025-11-08 22:40:34 +0530

    Add diamond shape pattern printing program in diamondShape.c

---
+++ logic/dimondShape.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main() {
+    int count = 6;
+    for (int i = 1; i <= count; i++) {
+
+        for (int j = 1; j <= count-i; j++) {
+            printf(" ");
+        }
+
+        for (int k = 1; k <= 2*i-1; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (int i = count -1; i >= 1; i--) {
+
+        for (int j = 1; j <= count-i; j++) {
+            printf(" ");
+        }
+
+        for (int k = 1; k <= 2*i-1; k++) {
+            printf("*");
+        }
+        printf("\n");   
+    }
+}

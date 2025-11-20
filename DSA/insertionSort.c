3c4bc4f81a6038284ba1284e29c06ba1ca1526cb
mrcoder36 <mr.coder36@gmail.com>
2025-11-20 17:00:19 +0530

    Fix insertion sort comparison logic to sort in ascending order

--- DSA/insertionSort.c
+++ DSA/insertionSort.c
@@ -6,7 +6,7 @@ void insertionSort(int arr[], int n)
     for (i = 1; i < n; i++)
     {
         key = arr[i];
-        for (j = i - 1; j >= 0 && arr[j] < key; j--)
+        for (j = i - 1; j >= 0 && arr[j] > key; j--)
         {
             arr[j + 1] = arr[j];
         }

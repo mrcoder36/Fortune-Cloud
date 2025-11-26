4821fa9f8f8d9e3e48a1d246f37f70e33b3769f4
mrcoder36 <mr.coder36@gmail.com>
2025-11-26 11:44:13 +0530

    Add data types, if-else, and switch-case examples in JavaScript

---
+++ JS/dataTypes.js
@@ -0,0 +1,18 @@
+var a = 20;
+var b = 'Vishal'
+var arr = [1,2,3,4,5,6];
+var strArr = ['apple', 'banana', 'cherry', 'date'];
+var dec = 2.3
+var un;
+
+
+console.log(typeof(a));
+console.log(typeof(strArr));
+console.log(typeof(strArr));
+console.log(un);
+
+function greet() {
+    console.log("Hello World! " + b); 
+}
+greet();
+
